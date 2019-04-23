fa19c32b9b76fa5b17a09059a31c01225dec8d64
yvt <i@yvt.jp>
2019-04-23 15:27:45 +0900

    Use `cfg_if` to select a backend

--- tcw3/src/pal/mod.rs
+++ tcw3/src/pal/mod.rs
@@ -1,15 +1,21 @@
 //! Platform abstraction layer
+use cfg_if::cfg_if;
+
 pub mod traits;
 pub mod types;
 
 #[doc(no_inline)]
 pub use self::types::*;
 
-pub mod macos;
-// TODO: Other platforms
+cfg_if! {
+    if #[cfg(target_os = "macos")] {
+        pub mod macos;
 
-/// The default window manager type for the target platform.
-pub type WM = macos::WM;
+        /// The default window manager type for the target platform.
+        pub type WM = macos::WM;
+    }
+    // TODO: Other platforms
+}
 
 /// Get the default instance of [`WM`]. It only can be called by a main thread.
 pub fn wm() -> &'static WM {
