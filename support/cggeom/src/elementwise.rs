f71b0498cffb7507f39646191824c1ff7053c598
yvt <i@yvt.jp>
2019-05-26 09:20:26 +0900

    Implement `ElementWiseOp` on `Vector2`, etc.

--- support/cggeom/src/elementwise.rs
+++ support/cggeom/src/elementwise.rs
@@ -1,4 +1,4 @@
-use cgmath::{BaseNum, Point2, Point3};
+use cgmath::{BaseNum, Point2, Point3, Vector2, Vector3, Vector4};
 use std::cmp::Ordering;
 
 pub trait BoolArray {
@@ -62,6 +62,51 @@ impl<T: BaseNum> ElementWiseOp for Point3<T> {
     }
 }
 
+impl<T: BaseNum> ElementWiseOp for Vector2<T> {
+    fn element_wise_min(&self, rhs: &Self) -> Self {
+        Self::new(num_min(self.x, rhs.x), num_min(self.y, rhs.y))
+    }
+    fn element_wise_max(&self, rhs: &Self) -> Self {
+        Self::new(num_max(self.x, rhs.x), num_max(self.y, rhs.y))
+    }
+}
+
+impl<T: BaseNum> ElementWiseOp for Vector3<T> {
+    fn element_wise_min(&self, rhs: &Self) -> Self {
+        Self::new(
+            num_min(self.x, rhs.x),
+            num_min(self.y, rhs.y),
+            num_min(self.z, rhs.z),
+        )
+    }
+    fn element_wise_max(&self, rhs: &Self) -> Self {
+        Self::new(
+            num_max(self.x, rhs.x),
+            num_max(self.y, rhs.y),
+            num_max(self.z, rhs.z),
+        )
+    }
+}
+
+impl<T: BaseNum> ElementWiseOp for Vector4<T> {
+    fn element_wise_min(&self, rhs: &Self) -> Self {
+        Self::new(
+            num_min(self.x, rhs.x),
+            num_min(self.y, rhs.y),
+            num_min(self.z, rhs.z),
+            num_min(self.w, rhs.w),
+        )
+    }
+    fn element_wise_max(&self, rhs: &Self) -> Self {
+        Self::new(
+            num_max(self.x, rhs.x),
+            num_max(self.y, rhs.y),
+            num_max(self.z, rhs.z),
+            num_max(self.w, rhs.w),
+        )
+    }
+}
+
 impl<T: PartialOrd> ElementWisePartialOrd for Point2<T> {
     type Bool = [bool; 2];
     fn element_wise_gt(&self, rhs: &Self) -> Self::Bool {
@@ -94,6 +139,74 @@ impl<T: PartialOrd> ElementWisePartialOrd for Point3<T> {
     }
 }
 
+impl<T: PartialOrd> ElementWisePartialOrd for Vector2<T> {
+    type Bool = [bool; 2];
+    fn element_wise_gt(&self, rhs: &Self) -> Self::Bool {
+        [self.x > rhs.x, self.y > rhs.y]
+    }
+    fn element_wise_lt(&self, rhs: &Self) -> Self::Bool {
+        [self.x < rhs.x, self.y < rhs.y]
+    }
+    fn element_wise_ge(&self, rhs: &Self) -> Self::Bool {
+        [self.x >= rhs.x, self.y >= rhs.y]
+    }
+    fn element_wise_le(&self, rhs: &Self) -> Self::Bool {
+        [self.x <= rhs.x, self.y <= rhs.y]
+    }
+}
+
+impl<T: PartialOrd> ElementWisePartialOrd for Vector3<T> {
+    type Bool = [bool; 3];
+    fn element_wise_gt(&self, rhs: &Self) -> Self::Bool {
+        [self.x > rhs.x, self.y > rhs.y, self.z > rhs.z]
+    }
+    fn element_wise_lt(&self, rhs: &Self) -> Self::Bool {
+        [self.x < rhs.x, self.y < rhs.y, self.z < rhs.z]
+    }
+    fn element_wise_ge(&self, rhs: &Self) -> Self::Bool {
+        [self.x >= rhs.x, self.y >= rhs.y, self.z >= rhs.z]
+    }
+    fn element_wise_le(&self, rhs: &Self) -> Self::Bool {
+        [self.x <= rhs.x, self.y <= rhs.y, self.z <= rhs.z]
+    }
+}
+
+impl<T: PartialOrd> ElementWisePartialOrd for Vector4<T> {
+    type Bool = [bool; 4];
+    fn element_wise_gt(&self, rhs: &Self) -> Self::Bool {
+        [
+            self.x > rhs.x,
+            self.y > rhs.y,
+            self.z > rhs.z,
+            self.w > rhs.w,
+        ]
+    }
+    fn element_wise_lt(&self, rhs: &Self) -> Self::Bool {
+        [
+            self.x < rhs.x,
+            self.y < rhs.y,
+            self.z < rhs.z,
+            self.w < rhs.w,
+        ]
+    }
+    fn element_wise_ge(&self, rhs: &Self) -> Self::Bool {
+        [
+            self.x >= rhs.x,
+            self.y >= rhs.y,
+            self.z >= rhs.z,
+            self.w >= rhs.w,
+        ]
+    }
+    fn element_wise_le(&self, rhs: &Self) -> Self::Bool {
+        [
+            self.x <= rhs.x,
+            self.y <= rhs.y,
+            self.z <= rhs.z,
+            self.w <= rhs.w,
+        ]
+    }
+}
+
 impl BoolArray for [bool; 2] {
     #[inline]
     fn any(&self) -> bool {
@@ -113,5 +226,16 @@ impl BoolArray for [bool; 3] {
     #[inline]
     fn all(&self) -> bool {
         self[0] && self[1] && self[2]
+    }
+}
+
+impl BoolArray for [bool; 4] {
+    #[inline]
+    fn any(&self) -> bool {
+        self[0] || self[1] || self[2] || self[3]
+    }
+    #[inline]
+    fn all(&self) -> bool {
+        self[0] && self[1] && self[2] && self[3]
     }
 }
