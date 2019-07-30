cada326f4191b587ab21907ffb0e581a4ac36ec7
yvt <i@yvt.jp>
2019-07-30 15:18:31 +0900

    Add an example `tcw3_table`

---
+++ tcw3/examples/tcw3_table.rs
@@ -0,0 +1,66 @@
+use std::ops::Range;
+use tcw3::{
+    pal,
+    pal::prelude::*,
+    ui::{
+        layouts::FillLayout,
+        prelude::*,
+        theming,
+        views::{table, table::LineTy, Label, Table},
+    },
+    uicore::{HView, HWnd, SizeTraits, WndListener},
+};
+
+struct MyWndListener;
+
+impl WndListener for MyWndListener {
+    fn close(&self, wm: pal::WM, _: &HWnd) {
+        wm.terminate();
+    }
+}
+
+struct TableModelQuery {
+    style_manager: &'static theming::Manager,
+}
+
+impl table::TableModelQuery for TableModelQuery {
+    fn new_view(&mut self, cell: table::CellIdx) -> (HView, Box<dyn table::CellCtrler>) {
+        let mut label = Label::new(self.style_manager);
+        label.set_text(format!("{:?}", cell));
+
+        (label.view().clone(), Box::new(()))
+    }
+
+    fn range_size(&mut self, _line_ty: LineTy, range: Range<u64>, _approx: bool) -> f64 {
+        (range.end - range.start) as f64 * 50.0
+    }
+}
+
+fn main() {
+    let wm = pal::WM::global();
+    let style_manager = theming::Manager::global(wm);
+
+    let wnd = HWnd::new(wm);
+    wnd.set_visibility(true);
+    wnd.set_listener(MyWndListener);
+
+    let table = Table::new();
+
+    table.set_size_traits(SizeTraits {
+        preferred: [200.0, 300.0].into(),
+        ..Default::default()
+    });
+
+    // Set up the table model
+    {
+        let mut edit = table.edit().unwrap();
+        edit.set_model(TableModelQuery { style_manager });
+        edit.insert(LineTy::Row, 0..500);
+        edit.insert(LineTy::Col, 0..300);
+    }
+
+    wnd.content_view()
+        .set_layout(FillLayout::new(table.view().clone()).with_uniform_margin(10.0));
+
+    wm.enter_main_loop();
+}
