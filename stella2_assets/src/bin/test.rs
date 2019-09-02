0c30cc84464bb5802b384bd126ecea6b655d520d
yvt <i@yvt.jp>
2019-09-02 07:58:24 -0700

    Delete `stella2-windres-test`

--- stella2_assets/src/bin/test.rs
+++
@@ -1,8 +0,0 @@
-// This executable is used to test the generation of the Windows resource
-// done by `build.rs`. It literally does nothing and only used to be
-// manually inspected by developers.
-fn main() {}
-
-// Make sure `stella2_assets` is linked
-#[used]
-static X: &'static stella2_assets::Stvg = &stella2_assets::toolbar::GO_BACK;
