15b5823d6fec0eefb4e542d9f798bb5935c5150f
yvt <i@yvt.jp>
2019-12-19 11:24:27 +0900

    Add crate `fimvm`

---
+++ fimvm/src/lib.rs
@@ -0,0 +1,10 @@
+//! fimVM (Fast, Interpreted, Minimum Virtual Machine), a virtual machine
+//! for integration with the translation system.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
