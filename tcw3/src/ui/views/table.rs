yvt/Stella2

commit f861761c58939e777f692cbb2aad4fe049d60098

Improve `TableModelEditExt::model_downcast_mut`'s type safety

tcw3/src/ui/views/table.rs
@@ -439,7 +439,7 @@ pub trait TableModelEditExt: TableModelEdit {
     ///         .expect("wrong concrete type");
     ///     # }
     ///
-    fn model_downcast_mut<T: Any>(&mut self) -> Option<&mut T> {
+    fn model_downcast_mut<T: TableModelQuery>(&mut self) -> Option<&mut T> {
         (*self.model_mut()).as_any_mut().downcast_mut()
     }
 }

commit c96dca021d9177baa3b729164f72e20b54f00ed1

Fix doctest
The test was broken by the commit
f861761c58939e777f692cbb2aad4fe049d60098.

tcw3/src/ui/views/table.rs
@@ -442,10 +442,9 @@ pub trait TableModelEditExt: TableModelEdit {
     /// Downcast the result of `self.model_mut()`.
     ///
     ///     # use tcw3::ui::views::table::*;
-    ///     # fn test(
+    ///     # fn test<MyModelQuery: TableModelQuery>(
     ///     #    edit: &mut dyn TableModelEdit,
     ///     # ) {
-    ///     # type MyModelQuery = Vec<f32>;
     ///     let my_model: &mut MyModelQuery = edit.model_downcast_mut()
     ///         .expect("wrong concrete type");
     ///     # }