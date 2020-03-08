500883506d4844fe46bf05c37997237061c4215b
yvt <i@yvt.jp>
2020-03-08 17:49:05 +0900

    feat(pal): re-export some items

--- tcw3/pal/src/lib.rs
+++ tcw3/pal/src/lib.rs
@@ -19,7 +19,7 @@ pub mod prelude {
     pub use super::cells::MtLazyStatic;
     pub use super::iface::{
         Bitmap, BitmapBuilder, BitmapBuilderNew, Canvas, CanvasText, CharStyle, MouseDragListener,
-        ScrollListener, TextLayout, Wm, WndListener,
+        ScrollListener, TextInputCtxEdit, TextInputCtxListener, TextLayout, Wm, WndListener,
     };
 
     pub use super::futuresext::WmFuturesExt;
@@ -124,8 +124,8 @@ pub type TextLayout = current::TextLayout;
 // the default backend.
 
 pub use self::iface::{
-    BadThread, CursorShape, LayerFlags, LineCap, LineJoin, ScrollDelta, SysFontType,
-    TextDecorFlags, WndFlags, RGBAF32,
+    BadThread, CursorShape, IndexFromPointFlags, LayerFlags, LineCap, LineJoin, ScrollDelta,
+    SysFontType, TextDecorFlags, TextInputCtxEventFlags, WndFlags, RGBAF32,
 };
 
 /// The window handle type of [`Wm`].
