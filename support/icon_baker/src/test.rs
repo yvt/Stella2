cf46af0baaf7df9ecb9c9eda90145aa76e3ffceb
yvt <i@yvt.jp>
2020-03-31 00:09:26 +0900

    test(icon_baker): use a file from this repository as input

--- support/icon_baker/src/test.rs
+++ support/icon_baker/src/test.rs
@@ -18,6 +18,11 @@ macro_rules! png {
     };
 }
 
+fn input_svg_image_path() -> PathBuf {
+    let manifest_dir = Path::new(env!("CARGO_MANIFEST_DIR"));
+    manifest_dir.join("tests/deref.svg")
+}
+
 #[test]
 fn test_resample() {
     let mut file_near = File::create("tests/test_near.png").expect("Couldn't create file");
@@ -26,7 +31,7 @@ fn test_resample() {
 
     let mut file_cubic = File::create("tests/test_cubic.png").expect("Couldn't create file");
 
-    let img = SourceImage::from_path("tests/hydra.png").expect("File not found");
+    let img = SourceImage::from_path(input_svg_image_path()).expect("File not found");
 
     png!(resample::nearest, &img, &mut file_near);
     png!(resample::linear, &img, &mut file_linear);
@@ -38,7 +43,7 @@ fn test_ico() {
     let mut file = BufWriter::new(File::create("tests/test.ico").expect("Couldn't create file"));
 
     let mut icon = Ico::new();
-    let img = SourceImage::from_path("tests/hydra.png").expect("File not found");
+    let img = SourceImage::from_path(input_svg_image_path()).expect("File not found");
 
     if let Err(err) = icon.add_entries(resample::nearest, &img, vec![32, 64]) {
         panic!("{:?}", err);
@@ -62,7 +67,7 @@ fn test_icns() {
     let mut file = BufWriter::new(File::create("tests/test.icns").expect("Couldn't create file"));
 
     let mut icon = Icns::new();
-    let img = SourceImage::from_path("tests/hydra.png").expect("File not found");
+    let img = SourceImage::from_path(input_svg_image_path()).expect("File not found");
 
     if let Err(err) = icon.add_entries(resample::nearest, &img, vec![32, 64]) {
         panic!("{:?}", err);
