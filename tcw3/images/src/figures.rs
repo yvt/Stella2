c80963c10468e29d40ffd640b8565ea673e1c365
yvt <i@yvt.jp>
2020-04-03 01:19:46 +0900

    fix(images): make 9-slice images slightly larger
    Fixes edges being too close to 9-slice scaling seams and artifacts
    appearing when the given corner radii are zero.

--- tcw3/images/src/figures.rs
+++ tcw3/images/src/figures.rs
@@ -72,7 +72,7 @@ pub fn himg_from_figures(figures: impl Borrow<[Figure]> + Send + Sync + 'static)
             shuffle!(margins, [3, 2, 0, 0]),
         ]
         .fmax();
-        let size = size * 2.0 + 1.0;
+        let size = size * 2.0 + 2.0;
 
         [size.extract(0), size.extract(1)]
     }
