e0db25c67b47db3ec242a4f57a352fb1538e1782
yvt <i@yvt.jp>
2020-04-17 02:00:41 +0900

    test(designer): make `tcw3codegen` usable again

--- tcw3/designer/src/bin/tcw3codegen.rs
+++ tcw3/designer/src/bin/tcw3codegen.rs
@@ -5,13 +5,14 @@ use tcw3_designer::BuildScriptConfig;
 fn main() {
     let args: Vec<_> = env::args_os().collect();
 
-    if args.len() != 3 {
-        eprintln!("Usage: tcw3codegen INPUT.tcwdl OUTPUT.rs");
+    if args.len() != 4 {
+        eprintln!("Usage: tcw3codegen INPUT.tcwdl CRATENAME OUTPUT.rs");
         std::process::exit(1);
     }
 
     BuildScriptConfig::new()
         .root_source_file(&args[1])
-        .out_source_file(&args[2])
+        .crate_name(args[2].to_str().expect("Crate name contains invalid UTF-8"))
+        .out_source_file(&args[3])
         .run_and_exit_on_error();
 }
